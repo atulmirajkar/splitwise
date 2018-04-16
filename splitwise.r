f873befe416e159d3d81b65c2b202911cb62a7d0
atul <atulmirajkar@gmail.com>
2018-04-15 21:01:26 -0500

    add category

--- splitwise.r
+++ splitwise.r
@@ -5,136 +5,136 @@ library(shiny)
 
 rm(list=ls())
 
-setwd("C:/Users/atulm/work/RCode/splitwise")
+setwd("C:/Users/atulm/RCode/Splitwise")
 
 
-#import data
-origDF = read.csv("province_export.csv",as.is = T)
-
-#remove na
-origDF<-origDF[!(is.na(origDF$Date)),]
-origDF<-origDF[!(origDF$Description=="Total balance"),]
-
-#structure
-origDF$Date<-as.POSIXct(origDF$Date)
-origDF$Category<-as.factor(origDF$Category)
-
-
-#View rent / internet / netflix
-View(origDF[origDF$Category=="Rent",])
-View(origDF[grep("Netflix*",origDF$Description,ignore.case = TRUE),])
-View(origDF[grep("*charter*",origDF$Description,ignore.case = TRUE),])
-
-
-
-#quick
-levels(origDF$Category)
-str(origDF)
-View(origDF)
 
 #shiny
-shinyServer<-function(input,output){
+shinyServer<-function(input,output,session){
+  #read data
+  mydata<-reactive({
+    
+    origDF = read.csv("expenses.csv",as.is = T)
+    
+    #remove na
+    origDF<-origDF[!(is.na(origDF$Date)),]
+    origDF<-origDF[!(origDF$Description=="Total balance"),]
+    
+    #structure
+    origDF$Date<-as.POSIXct(origDF$Date)
+    origDF$Category<-as.factor(origDF$Category)
+    origDF$Group<-as.factor(origDF$Group)
+    
+    #allGroupDF
+    #drop currency
+    allGroupDF<-origDF[,!(names(origDF) %in% c("X"))]
+    
+    #add month and year
+    allGroupDF$year<-year(allGroupDF$Date)
+    allGroupDF$month<-month(allGroupDF$Date)
+    allGroupDF$day<-day(allGroupDF$Date)
+    
+    #remove total balance and cost as double
+    allGroupDF$Cost<-as.double(allGroupDF$Cost)
+    allGroupDF$Share<-as.double(allGroupDF$Share)
+    return(allGroupDF)
+  })
   
-  output$dateRangeText2 <- renderText({
+  output$dateRangeText2<-renderText({
     paste(input$dateRange[1],input$dateRange[2])
     
   })
- 
-
-  output$distPlot1 <- renderPlot({
+  
+  inputCategory<-reactive({
+    data=mydata()
+    levels(data$Category)
+    
+  })
+  
+  inputGroup<-reactive({
+    data=mydata()
+    levels(data$Group)
+    
+  })
+  
+  observeEvent(mydata(),{
+    updateCheckboxGroupInput(session, "category", choices = inputCategory())
+    updateCheckboxGroupInput(session, "group", choices = inputGroup())
+  })
+  
+  
+  
+  
+  output$distPlot1<-renderPlot({
+    data=mydata()
     
     startDate<-as.POSIXct(input$dateRange[1])
     endDate<-as.POSIXct(input$dateRange[2])
-    subsetOrigDF<-origDF[origDF$Date>=input$dateRange[1] & origDF$Date<=input$dateRange[2],]
-    subsetOrigDF$Date<-as.character(subsetOrigDF$Date)
-    output$table <- renderTable(subsetOrigDF)
+    if(is.null(input$category) || is.null(input$group)) return(NULL)
     
-    #myshare
-    myShareDF<-melt(data=subsetOrigDF,id=c("Date","Description","Category","Cost","Currency"))
-    
-    #drop currency
-    colnames(myShareDF)<-c("Date","Description","Category","Cost","Currency","User","Share")
-    myShareDF<-myShareDF[,!(names(myShareDF) %in% c("Currency"))]
-    
-    #add month and year
-    myShareDF$year<-year(myShareDF$Date)
-    myShareDF$month<-month(myShareDF$Date)
-    myShareDF$day<-day(myShareDF$Date)
-    
-    #remove total balance and cost as double
-    myShareDF<-myShareDF[myShareDF$User!="Aniket",]
-    myShareDF$Cost<-as.double(myShareDF$Cost)
-    myShareDF$Share<-as.double(myShareDF$Share)
-    myShareDF$Share[myShareDF$Share<0]<-myShareDF$Share[myShareDF$Share<0]*-1
-    
-    catShareDF<-aggregate(myShareDF$Share,by=list(myShareDF$Category),FUN=sum)
-    colnames(catShareDF)<-c("category","cost")
+    data<-data[data$Date>=input$dateRange[1] & data$Date<=input$dateRange[2],]
     
     
-    ggplot(data=catShareDF[catShareDF$category %in% input$variable,], aes(x="", y=cost, fill=factor(category))) +
+    
+    catShareDF<-data[data$Category %in% input$category,]
+    catGroupShareDF<-catShareDF[catShareDF$Group %in% input$group,]
+    
+    if(nrow(catGroupShareDF)==0) return(NULL)
+    
+    ggplot(data=catGroupShareDF, aes(x="", y=Cost, fill=factor(Category))) +
       geom_bar(stat="identity",width=1)+
       coord_polar(theta = "y",start = 0)
   })
   
-  output$distPlot2 <- renderPlot({
+  output$distPlot2<-renderPlot({
+    data=mydata()
+    
     startDate<-as.POSIXct(input$dateRange[1])
     endDate<-as.POSIXct(input$dateRange[2])
-    subsetOrigDF<-origDF[origDF$Date>=input$dateRange[1] & origDF$Date<=input$dateRange[2],]
-    subsetOrigDF$Date<-as.character(subsetOrigDF$Date)
-    output$table <- renderTable(subsetOrigDF)
+    data<-data[data$Date>=input$dateRange[1] & data$Date<=input$dateRange[2],]
     
-    #myshare
-    myShareDF<-melt(data=subsetOrigDF,id=c("Date","Description","Category","Cost","Currency"))
+    if(is.null(input$category) || is.null(input$group)) return(NULL)
     
-    #drop currency
-    colnames(myShareDF)<-c("Date","Description","Category","Cost","Currency","User","Share")
-    myShareDF<-myShareDF[,!(names(myShareDF) %in% c("Currency"))]
+    catShareDF<-data[data$Category %in% input$category,]
+    catGroupShareDF<-catShareDF[catShareDF$Group %in% input$group,]
+    if(nrow(catGroupShareDF)==0) return(NULL)
     
-    #add month and year
-    myShareDF$year<-year(myShareDF$Date)
-    myShareDF$month<-month(myShareDF$Date)
-    myShareDF$day<-day(myShareDF$Date)
-    
-    #remove total balance and cost as double
-    myShareDF<-myShareDF[myShareDF$User!="Aniket",]
-    myShareDF$Cost<-as.double(myShareDF$Cost)
-    myShareDF$Share<-as.double(myShareDF$Share)
-    myShareDF$Share[myShareDF$Share<0]<-myShareDF$Share[myShareDF$Share<0]*-1
-    
-    
-    
-    monthlyShareDF<-aggregate(myShareDF$Share,by=list(myShareDF$Category,myShareDF$month),FUN = sum)
+    monthlyShareDF<-aggregate(catGroupShareDF$Share,by=list(catGroupShareDF$Category,catGroupShareDF$month),FUN = sum)
     colnames(monthlyShareDF)<-c("Category","Month","Share")
     
-    ggplot(data=monthlyShareDF[monthlyShareDF$Category %in% input$variable,], aes(x=Month, y=Share, fill=Category)) +
+    if(nrow(monthlyShareDF)==0) return(NULL)
+    
+    
+    ggplot(data=monthlyShareDF, aes(x=Month, y=Share, fill=Category)) +
       geom_bar(colour="black", stat="identity")+
       scale_x_continuous(breaks = seq(1,12,by=1))
     
   })
 }
 
-shinyUI <- fluidPage(
+shinyUI<-fluidPage(
   pageWithSidebar(
-  
-  # Application title
+    
+    # Application title
     headerPanel("Splitwise Analytics!"),
-  
+    
     # Sidebar with a slider input for number of observations
     sidebarPanel(
       dateRangeInput('dateRange',
-                   label = 'Select start and end date',
-                   start = Sys.Date() - 2, end = Sys.Date()
+                     label = 'Select start and end date',
+                     start = Sys.Date() - 2, end = Sys.Date()
       ),
-      checkboxGroupInput("variable", "Variables to show:",
-                       levels(monthlyShareDF$Category),choices =levels(monthlyShareDF$Category) )
-  ),
-  
-  # Show a plot of the generated distribution
-  mainPanel(
-    plotOutput("distPlot1"),
-    plotOutput("distPlot2")
+      checkboxGroupInput("category", "categorys to show:"),
+      
+      checkboxGroupInput("group", "groups to show:")
+    ),
     
-  )
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot1"),
+      plotOutput("distPlot2")
+      
+    )
   ),
   fluidPage(
     tableOutput('table')
@@ -142,4 +142,4 @@ shinyUI <- fluidPage(
   
 )
 
-shinyApp(ui = shinyUI, server = shinyServer) # this launches your app
+shinyApp(ui = shinyUI, server = shinyServer)
